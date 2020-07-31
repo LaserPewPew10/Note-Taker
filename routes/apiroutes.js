18ae10a8c7ae12b8271560905e61c7f5b065433e
Justin Ramirez <ramirezbjustin@gmail.com>
2020-07-31 19:59:46 -0400

    added in the delete api route

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,6 +1,7 @@
 // DEPENDECIES ===================================
 const fs = require("fs");
 const path = require("path");
+const { json } = require("express");
 
 module.exports = function (app) {
   // API ROUTES =================================
@@ -26,8 +27,13 @@ module.exports = function (app) {
         var newNote = req.body;
         // Proposition: We want each new id to be one greater than the last elements id
         // newNote.id = database.length + 1;
-        const lastElementId = database[database.length - 1].id;
-        newNote.id = lastElementId + 1;
+        // If there are no notes, you end up breaking because you can't make a new note id.
+        if (database.length === 0) {
+          newNote.id = 1;
+        } else {
+          const lastElementId = database[database.length - 1].id;
+          newNote.id = lastElementId + 1;
+        }
         database.push(newNote);
         database = JSON.stringify(database);
         fs.writeFile("db/db.json", database, function (err) {
@@ -36,8 +42,29 @@ module.exports = function (app) {
         });
       });
     });
-    // setting up for /api/notes post route
     // DELETE API ==================================
     // setting up for /api/notes/:id
+    app.delete("/api/notes/:id", function (req, res) {
+      const id = parseInt(req.params.id);
+
+      fs.readFile("db/db.json", "utf8", function (err, database) {
+        // if there is an error based off of reading the file, the file will be thrown out by using the thrown syntax
+        if (err) throw err;
+
+        database = JSON.parse(database);
+
+        // filter keeps evertyhing for which the function in filter returns true.
+        var newDataBase = database.filter((note) => {
+          return note.id !== id;
+        });
+
+        newDataBase = JSON.stringify(newDataBase);
+
+        fs.writeFile("db/db.json", newDataBase, function (err) {
+          if (err) throw err;
+          res.sendStatus(200);
+        });
+      });
+    });
   });
 };
