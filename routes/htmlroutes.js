1146c47051292ce557c4489d8a9287c09c57e708
Justin Ramirez <ramirezbjustin@gmail.com>
2020-07-31 13:07:24 -0400

    looked into package json file

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,12 +1,14 @@
+// DEPENDECIES ===================================
 const path = require("path");
-// Display notes.html
+
+// Display notes.html while using the GET METHOD
 module.exports = function (app) {
-  app.get("*", function (req, res) {
-    res.sendfile(path.join(__dirname, "../public/index.html"));
+  // display notes.html while using the GET METHOD
+  app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
 
-  // display notes.html
-  app.get("/notes", function (req, res) {
-    res.sendfile(path.join(__dirname, "../public/notes.html"));
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 };
